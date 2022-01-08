7170df8b5740930207514526d4f6c4ef01c1240f
Artem1441 <artem.volcano@gmail.com>
2022-01-08 15:10:58 +0500

    second commit

--- index.js
+++ index.js
@@ -9,11 +9,17 @@ app.use(cors());
 const data = [
   { id: 1, text: "aaa" },
   { id: 2, text: "bbb" },
+  { id: 3, text: "ccc" },
 ];
 
 app.get("/data", (req, res) => {
   res.send(JSON.stringify(data));
 });
+
+app.get("/sayHello", (req, res) => {
+    res.send("Hello all!");
+  });
+  
 
 app.listen(5000, () => {
   console.log("Server Started");
